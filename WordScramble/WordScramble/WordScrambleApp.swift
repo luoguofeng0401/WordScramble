27c9d8d1856d3fb789d4b89613a4c874b11c01fc
Guofeng Luo <56261249+luoguofeng0401@users.noreply.github.com>
2026-04-22 09:07:31 +0800

    Add files via upload

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Guofeng Luo on 2026-04-22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
